583e8804c17f253a835df4925b672d06a3a161f1
keryfyN <kerafyn1up@gmail.com>
2015-08-31 15:54:56 +0200

    retour sur le beurre

--- runserver.py
+++ runserver.py
@@ -4,5 +4,5 @@ from application import app
 # run the application!
 if __name__ == '__main__':
      # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 6000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
